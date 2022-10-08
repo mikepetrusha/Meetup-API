f55f96e729ec9948ed325c4399fbd2074fa157df
Mike Petrusha <petrushamike@gmail.com>
2022-10-08 16:55:09 +0300

    feat: add pagination and search

--- controllers/meetupController.js
+++ controllers/meetupController.js
@@ -4,7 +4,32 @@ const { meetupDTO } = require('../dto/meetup.dto')
 
 class MeetupController {
     async getAll(req, res) {
-        const meetups = await meetupDB.findAll()
+        const { title, keywords, limit, page, sort } = req.query
+
+        async function getMeetups(title, keywords, limit = 5, page = 1, sort = 0) {
+            let meetups
+            let offset = (page - 1) * limit
+
+            if (!title && !keywords) {
+                if (sort) { return meetups = await meetupDB.findAll({ order: [['id', 'ASC']], limit, offset }) }
+                return meetups = await meetupDB.findAll({ limit, offset })
+            }
+            if (title && !keywords) {
+                if (sort) { return meetups = await meetupDB.findAll({ where: { title }, order: [["id", 'ASC']], limit, offset }) }
+                return meetups = await meetupDB.findAll({ where: { title }, limit, offset })
+            }
+            if (!title && keywords) {
+                if (sort) { return meetups = await meetupDB.findAll({ where: { keywords }, order: [["id", 'ASC']], limit, offset }) }
+                return meetups = await meetupDB.findAll({ where: { keywords }, limit, offset })
+            }
+            if (title && keywords) {
+                if (sort) { return meetups = await meetupDB.findAll({ where: { title, keywords }, order: [["id", 'ASC']], limit, offset }) }
+                return meetups = await meetupDB.findAll({ where: { title, keywords }, limit, offset })
+            }
+            return meetups
+        }
+
+        const meetups = await getMeetups(title, keywords, limit, page, sort)
         return res.json(meetups)
     }
 
