4260656ad07bc69fd27ba8fbbb6bdee98d856e77
Mike Petrusha <petrushamike@gmail.com>
2022-10-08 11:27:52 +0300

    feat: add joi validation for meetups

--- controllers/meetupController.js
+++ controllers/meetupController.js
@@ -1,4 +1,5 @@
 const { meetupDB } = require('../models/models')
+const { meetupDTO } = require('../dto/meetup.dto')
 
 
 class MeetupController {
@@ -6,22 +7,32 @@ class MeetupController {
         const meetups = await meetupDB.findAll()
         return res.json(meetups)
     }
+
     async getOne(req, res) {
         const { id } = req.params
         const meetup = await meetupDB.findOne({ where: { id } })
         return res.json(meetup)
     }
+
     async create(req, res) {
+        try {
+            await meetupDTO.validateAsync(req.body)
+        } catch (error) {
+            return res.send("The paremetrs are set incorrectly. All parametrs must be a string.")
+        }
+
         const { title, description, keywords, eventInformation } = req.body
         const meetup = await meetupDB.create({ title, description, keywords, eventInformation })
         return res.json(meetup)
     }
+
     async update(req, res) {
         const { id } = req.params
         const { title, description, keywords, eventInformation } = req.body
         await meetupDB.update({ title, description, keywords, eventInformation }, { where: { id } })
         return res.send('Meetup was updated')
     }
+
     async delete(req, res) {
         const { id } = req.params
         await meetupDB.destroy({ where: { id } })
---
+++ dto/meetup.dto.js
@@ -0,0 +1,10 @@
+const Joi = require('joi')
+
+const meetupDTO = Joi.object({
+    title: Joi.string(),
+    description: Joi.string(),
+    keywords: Joi.string(),
+    eventInformation: Joi.string(),
+})
+
+module.exports = meetupDTO
