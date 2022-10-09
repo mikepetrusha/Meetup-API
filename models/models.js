04acc210d3274717d949e7bd303cba84c9ca8b29
Mike Petrusha <petrushamike@gmail.com>
2022-10-09 15:21:41 +0300

    feat: add user to model file

--- models/models.js
+++ models/models.js
@@ -26,4 +26,25 @@ const meetupDB = sequelize.define('meetup', {
 
 })
 
-module.exports = { meetupDB }
+const userDB = sequelize.define('user', {
+    id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true
+    },
+    email: {
+        type: DataTypes.STRING,
+        unique: true,
+        allowNull: false
+    },
+    password: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    role: {
+        type: DataTypes.STRING,
+        defaultValue: "USER"
+    }
+})
+
+module.exports = { meetupDB, userDB }
