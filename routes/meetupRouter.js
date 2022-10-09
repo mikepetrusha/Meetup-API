336de6942c178f8c46d2c5b7b6c1fc5226a7c29c
Mike Petrusha <petrushamike@gmail.com>
2022-10-09 16:59:17 +0300

    feat: add auth Middleware

--- routes/meetupRouter.js
+++ routes/meetupRouter.js
@@ -1,11 +1,12 @@
 const Router = require('express')
 const router = new Router()
 const meetupController = require('../controllers/meetupController')
+const authMiddleware = require('../middleware/authMiddleware')
 
 router.get('/', meetupController.getAll)
 router.get('/:id', meetupController.getOne)
-router.post('/', meetupController.create)
-router.patch('/:id', meetupController.update)
-router.delete('/:id', meetupController.delete)
+router.post('/', authMiddleware("ADMIN"), meetupController.create)
+router.patch('/:id', authMiddleware("ADMIN"), meetupController.update)
+router.delete('/:id', authMiddleware("ADMIN"), meetupController.delete)
 
 module.exports = router
