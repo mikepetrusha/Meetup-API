9bb681a60ec6337346e9ea2ef1ef91c5fe6b8fab
Mike Petrusha <petrushamike@gmail.com>
2022-10-09 15:23:43 +0300

    feat: add joi validation for user

---
+++ dto/user.dto.js
@@ -0,0 +1,11 @@
+const Joi = require('joi')
+
+const userDTO = Joi.object({
+    email: Joi.string().email({ minDomainSegments: 2, tlds: { allow: ['com', 'net'] } }),
+    password: Joi.string().pattern(new RegExp('^[a-zA-Z0-9]{3,30}$')),
+    role: Joi.string(),
+})
+
+module.exports = {
+    userDTO
+}
